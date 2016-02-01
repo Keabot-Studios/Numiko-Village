91846a1a0bb7cf8652d4ba19244f3f8acba92467
Richard Abbott <richie0415@hotmail.com>
2016-02-01 16:38:00 -0500

    Inital Commit

---
+++ Numiko Village Utility/src/net/keabotstudios/projectpickman/util/Utility.java
@@ -0,0 +1,5 @@
+package net.keabotstudios.projectpickman.util;
+
+public class Utility {
+
+}
---
+++ Numiko Village/src/net/keabotstudios/projectpickman/Game.java
@@ -0,0 +1,9 @@
+package net.keabotstudios.projectpickman;
+
+public class Game {
+
+	public static void main(String[] args) {
+
+	}
+
+}
