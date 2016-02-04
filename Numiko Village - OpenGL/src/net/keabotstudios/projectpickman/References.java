536767c02f776551bb392211282c74c5d60559dd
Richard Abbott <richie0415@hotmail.com>
2016-02-03 20:57:42 -0500

    Deleted old directories

--- Numiko Village - OpenGL/src/net/keabotstudios/projectpickman/References.java
+++
@@ -1,9 +0,0 @@
-package net.keabotstudios.projectpickman;
-
-public class References {
-	
-	public static final String NAME = "Numiko Village";
-	public static final int WIDTH = 1280;
-	public static final int HEIGHT = 720;
-
-}
