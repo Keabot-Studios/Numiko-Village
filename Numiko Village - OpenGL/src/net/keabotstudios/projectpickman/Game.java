816a56b0b33e4f01c79ce051a3232938b72814cd
Richard Abbott <richie0415@hotmail.com>
2016-02-03 20:40:50 -0500

    Added basic rendering infrastructure.

---
+++ Numiko Village - OpenGL/src/net/keabotstudios/projectpickman/Game.java
@@ -0,0 +1,60 @@
+package net.keabotstudios.projectpickman;
+
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.PixelFormat;
+
+import net.keabotstudios.projectpickman.util.ShaderUtils;
+
+public class Game implements Runnable {
+	
+	private boolean running = false;
+	private Thread thread;
+	
+	public void start() {
+		running = true;
+		thread = new Thread(this, References.NAME + " - Display");
+		thread.start();
+	}
+	
+	private void init() {
+		String version = glGetString(GL_VERSION);
+		System.out.println("OpenGL version:" + version);
+		
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	}
+
+	@Override
+	public void run() {
+		try {
+			Display.setDisplayMode(new DisplayMode(References.WIDTH, References.HEIGHT));
+			Display.setTitle(References.NAME);
+			ContextAttribs context = new ContextAttribs(3, 3);
+			if(System.getProperty("os.name").contains("Mac")) context = new ContextAttribs(3, 2);
+			Display.create(new PixelFormat(), context.withProfileCore(true));
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		init();
+		
+		while(running) {
+			render();
+			Display.update();
+			if(Display.isCloseRequested()) running = false;
+		}
+		Display.destroy();
+	}
+	
+	private void render() {
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+
+	public static void main(String args[]) {
+		new Game().start();
+	}
+	
+}
